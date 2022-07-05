bb3144aec70db18b977c5c7f6c970bc6fe7c33eb
andersoncassio23 <anderson.oliveira@uso.com.br>
2022-07-04 21:15:00 -0300

    First commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+import { Logo } from "../components/Logo";
+export function Header(){
+    return(
+        <header className="w-full py-5 flex items-center justify-center bg-gray-700">
+            <Logo/>
+        </header>
+    )
+}
